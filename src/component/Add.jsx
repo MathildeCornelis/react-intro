7c8ea6adf7ed637e7044dc90dc61ce44bf80c653
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-02-27 11:22:29 +0100

    design Title

---
+++ src/component/Add.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Add = () => {
+    return (
+        <div>
+            <p>add</p>
+        </div>
+    );
+};
+
+export default Add;
---
+++ src/component/Title.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+    return (
+        <>
+            <h1 className='font-sans text-3xl xl:text-5xl flex justify-center p-5 xl:p-10 bg-gray-200 border-b-2 border-black border-opacity-20'>
+                My Todo App
+            </h1>
+        </>
+    );
+};
+
+export default Title;
