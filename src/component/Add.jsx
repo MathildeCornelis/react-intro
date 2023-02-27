bad430d6b5c08eaa4495013eded8d230461f0b25
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-02-27 12:18:19 +0100

    design Add

--- src/component/Add.jsx
+++ src/component/Add.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Add = () => {
     return (
-        <section className='flex flex-col items-start xl:items-center m-5 font-sans text-l'>
+        <section className='flex flex-col items-start xl:items-center p-5 font-sans text-l border-b-2 border-black border-opacity-20'>
             <label htmlFor="" className='w-1/2 flex justify-start xl:justify-center pl-2 pt-2 pb-2 xl:text-2xl'>Write a new task</label>
             <input type="text" placeholder=' ...' className='w-1/2  flex justify-start ml-2 mt-2 mb-2 xl:text-2xl bg-gray-100 text-black border border-blue-100 rounded-md'/>
             <input type="button" value="Add" className='w-1/5 xl:w-1/2 flex justify-center ml-2 mt-2 mb-2 pt-0.5 pb-0.5 xl:text-2xl rounded-md bg-blue-800 text-gray-100'/>
