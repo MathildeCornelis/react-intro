f8f7d09293b63c76a198966abe3d6bc18b3a556c
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-02-28 14:22:00 +0100

    delete items checkbox

--- src/component/Parents.jsx
+++ src/component/Parents.jsx
@@ -1,44 +1,38 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Add from './Add';
-// import List from './List';
 
 const Parents = () => {
-    const [Value, setValue] = useState('');
+    const [value, setValue] = useState('');
     const [todos, setTodos] = useState([]);
 
     const handleAdd = (newValue) => {
-        setValue(newValue)
-        setTodos([...todos, newValue])
+        setValue(newValue);
+        setTodos([...todos, { text: newValue, checked: false }]);
     };
-    
-        
-    const [checked, setchecked] = useState(false);
-    const checkedValue = checked ? 'none' : 'flex';
+
+    const handleToggle = (index) => {
+        const newTodos = [...todos];
+        newTodos[index].checked = !newTodos[index].checked;
+        setTodos(newTodos);
+    };
 
     return (
         <>
-            <Add  onAdd={handleAdd}/>
+            <Add onAdd={handleAdd} />
             <section className='flex flex-col justify-center pt-4'>
                 <h2 className='w-full font-sans text-2xl xl:text-4xl flex flex-col items-center pb-4'>Todo</h2>
                 <ul className='flex flex-col pl-5 xl:pl-20'>
                     {todos.map((todo, index) => {
-                        // console.log(index);
-                        return(
-                            <li className='w-4/5 m-2 flex flex-row items-center' key={index} style={{ display: checkedValue}}>
-                                <input type="checkbox"  className='w-3 xl:w-4 h-3 xl:h-4 rounded-sm bg-blue-100 border-blue-300 text-blue-500 focus:ring-blue-800' 
-                                    onChange={() => {
-                                        // useState
-                                    }} 
-                                    onClick={() => setchecked(!checked)}
-                                />
-                                <p className='ml-3 xl:text-2xl'>{todo}</p>
+                        const checkedValue = todo.checked ? 'none' : 'flex';
+                        return (
+                            <li className='w-4/5 m-2 flex flex-row items-center'  key={index} style={{ display: checkedValue }} >
+                                <input type='checkbox' className='w-3 xl:w-4 h-3 xl:h-4 rounded-sm bg-blue-100 border-blue-300 text-blue-500 focus:ring-blue-800' checked={todo.checked} onChange={() => handleToggle(index)} />
+                                <p className='ml-3 xl:text-2xl'>{todo.text}</p>
                             </li>
-                            
-                        )
+                        );
                     })}
                 </ul>
             </section>
-            
         </>
     );
 };
